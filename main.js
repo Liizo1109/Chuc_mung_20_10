0152acf14e7ddd50e5bef97f1137f5df1181c528
Wynly._. <124950304+Liizo1109@users.noreply.github.com>
2024-10-20 19:49:37 +0700

    main.js

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+const flowers = ['🌸', '🏵️', '🌺', '🌻', '🌼', '🌷', '🌹', '🪻'];
+
+function createFlower() {
+  const flower = document.createElement('div');
+  flower.classList.add('flower');
+  flower.innerText = flowers[Math.floor(Math.random() * flowers.length)];
+  
+  // Đặt vị trí xuất hiện của hoa ngẫu nhiên theo chiều ngang (trục X)
+  flower.style.left = Math.random() * 100 + 'vw';
+  flower.style.animationDuration = Math.random() * 3 + 5 + 's'; // Thời gian hoa rơi
+  flower.style.fontSize = Math.random() * 10 + 15 + 'px'; // Kích thước hoa ngẫu nhiên
+
+  document.querySelector('.flower-falling-container').appendChild(flower);
+
+  setTimeout(() => {
+    flower.remove();
+  }, 5000);
+}
+
+setInterval(createFlower, 300);
